cf3526a8be4b73f501aad90312ceaaf966e5236f
shui00cc <384354037@qq.com>
2023-12-25 10:20:29 +0800

    fix:channel任务编排By Token

--- 06_channel_task/main.go
+++ 06_channel_task/main.go
@@ -9,41 +9,33 @@ package main
 
 import (
 	"fmt"
-	"sync"
 	"time"
 )
 
-const taskCount = 4
+var taskCount = 5
+
+type Token struct{}
+
+func newWorker(id int, ch chan Token, nextCh chan Token) {
+	for {
+		token := <-ch       // 取令牌
+		fmt.Println(id + 1) // 从1开始打印
+		time.Sleep(time.Second)
+		nextCh <- token
+	}
+}
 
 func main() {
-	var wg sync.WaitGroup
-	ch := make(chan int)
-
-	// 启动goroutine, num为编号, 从0开始
-	for num := 0; num < taskCount; num++ {
-		wg.Add(1)
-		go printTask(num, ch, &wg)
+	// 创建 taskCount 个channel
+	var chs []chan Token
+	for i := 0; i < taskCount; i++ {
+		chs = append(chs, make(chan Token))
 	}
-	// 通过channel发送信号，控制goroutine的执行顺序
-	for i := 0; i <= 10; i++ {
-		ch <- i % (taskCount)
-		time.Sleep(time.Second) // 每秒发送一个信号
+	// 启动newWorker
+	for id := 0; id < taskCount; id++ {
+		go newWorker(id, chs[id], chs[(id+1)%taskCount])
 	}
-
-	close(ch) // 关闭channel，通知所有goroutine退出
-	wg.Wait()
-}
-func printTask(num int, ch chan int, wg *sync.WaitGroup) {
-	defer wg.Done()
-	for {
-		select {
-		case n, ok := <-ch:
-			if !ok {
-				return // channel关闭时退出
-			}
-			if n == num { // 当收到自己的编号时打印
-				fmt.Println(num + 1)
-			}
-		}
-	}
+	// 初始token给第一个chan
+	chs[0] <- Token{}
+	select {}
 }
